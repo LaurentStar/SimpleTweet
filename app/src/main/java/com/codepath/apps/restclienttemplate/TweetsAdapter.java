31703a4eeec2cdc5d79a54e1580e3af899311599
Laurent_Vinn <laurent.mundell@gmail.com>
2019-11-01 13:30:24 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,93 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    Context context;
+    List<Tweet> tweets;
+
+
+    //Pass tweet list
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    // for each row blow up the layout
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent,false);
+        return new ViewHolder(view);
+    }
+
+    //bind value base on position of element
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Get the data at position
+        Tweet tweet = tweets.get(position);
+
+        //Bind the data
+        holder.bind(tweet);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+
+    // Clean all elements of the recycler
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+
+
+    //define view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivProfileImage;
+        TextView tvScreenName;
+        TextView tvBody;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvBody = itemView.findViewById(R.id.tvBody);
+        }
+
+        public void bind(Tweet tweet) {
+            Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+            tvScreenName.setText(tweet.user.screenName);
+            tvBody.setText(tweet.body);
+
+        }
+    }
+
+
+}
